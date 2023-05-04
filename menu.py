bae27992908e7174d0b38e48d5c3aaf69dd854c1
Feko89 <107096780+Feko89@users.noreply.github.com>
2023-05-04 18:40:41 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -9,11 +9,33 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-def display_button(img, x, y):
-    obr = pygame.image.load(img)
-    obr1=obr.get_rect()
-    screen.blit(obr, (x, y))
-    return obr1
+
+def get_button():
+
+    games_menu = pygame.image.load("minihry1.png").convert_alpha()
+    about_menu = pygame.image.load("onas1.png").convert_alpha()
+    end_menu = pygame.image.load("ukoncit1.png").convert_alpha()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 0
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if games_menu_rect.collidepoint(mouse_pos):
+                    return "Minihry"
+                elif about_menu_rect.collidepoint(mouse_pos):
+                    return "O nás"
+                elif end_menu_rect.collidepoint(mouse_pos):
+                    return 0
+                else:
+                    break
+        
+        games_menu_rect = screen.blit(games_menu, (320, 300))
+        about_menu_rect = screen.blit(about_menu, (320, 380))
+        end_menu_rect = screen.blit(end_menu, (320, 460))
+
+        pygame.display.flip()
 
 
 def main_menu():
@@ -21,28 +43,13 @@ def main_menu():
     clock = pygame.time.Clock()
     
     while menu:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                menu = False
-                pygame.quit()
-                quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if games_menu.collidepoint(mouse_pos):
-                    print("Ukáže menu s minihrami")
-                if about_menu.collidepoint(mouse_pos):
-                    print("ukáže info o nás")
-                if end_menu.collidepoint(mouse_pos):
-                    pygame.quit()
-                    quit()
-        
-        games_menu = display_button("minihry.png", 320, 300)
-        about_menu = display_button("onas.png", 320, 380) 
-        end_menu = display_button("ukoncit.png", 320, 460) 
-
-
+        button = get_button()
+        if button == 0:
+            menu = False
+            pygame.quit()
+            quit()
+        print(button)
         pygame.display.flip()
         clock.tick(60)
-        pygame.display.update()
                 
 main_menu()
